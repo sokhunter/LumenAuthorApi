c5f14ae5e5c3076368eda309f9235e5a970363f4
Stefany Livaque <emylivaque@gmail.com>
2020-06-11 17:30:04 -0500

    Crud de autores

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Author;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AuthorController extends Controller
 {
@@ -23,7 +25,8 @@ class AuthorController extends Controller
      */
     public function index()
     {
-        
+        $authors = Author::all();
+        return $this->successResponse($authors);
     }
 
     /**
@@ -32,33 +35,55 @@ class AuthorController extends Controller
      */
     public function store(Request $request)
     {
-        # code...
+        $rules = [
+            'name' => 'required|max:255',
+            'gender' => 'required|max:255|in:male, female',
+            'country' => 'required|max:255'
+        ];
+        $this->validate($request, $rules);
+        $author = Author::create($request->all());
+        return $this->successResponse($author, Response::HTTP_CREATED);
     }
 
     /**
      * Return a specific author
      * @return Illuminate\Http\Response
      */
-    public function show($author)
+    public function show($id)
     {
-        # code...
+        $author = Author::findOrFail($id);
+        return $this->successResponse($author);
     }
 
     /**
      * Update the information of an existing author
      * @return Illuminate\Http\Response
      */
-    public function update(Request $request, $author)
+    public function update(Request $request, $id)
     {
-        # code...
+        $rules = [
+            'name' => 'max:255',
+            'gender' => 'max:255|in:male, female',
+            'country' => 'max:255'
+        ];
+        $this->validate($request, $rules);
+        $author = Author::findOrFail($id);
+        $author->fill($request->all());
+        if ($author->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        $author->save();
+        return $this->successResponse($author);
     }
 
     /**
      * Removes an existing author
      * @return Illuminate\Http\Response
      */
-    public function destroy($author)
+    public function destroy($id)
     {
-        # code...
+        $author = Author::findOrFail($id);
+        $author->delete();
+        return $this->successResponse($author);
     }
 }
